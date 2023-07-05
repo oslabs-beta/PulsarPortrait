49edb11e5e99a89af86e9b48dbb8bf8d67c4f372
anthonyle910 <anthonyle910@yahoo.com>
2023-07-05 16:34:59 -0700

    added background image

--- client/components/Graph1.tsx
+++ client/components/Graph1.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Graph1() {
   return (
-    <div><iframe src="http://localhost:3000/d/f849b578-124a-4e19-a781-12d838580e64/messagein?orgId=1&refresh=5s&kiosk" width="1000em" height="250" frameBorder="0" ></iframe></div>
+    <div><iframe className='chart'src="http://localhost:3000/d/f849b578-124a-4e19-a781-12d838580e64/messagein?orgId=1&refresh=5s&viewPanel=1&kiosk"  frameBorder="0" ></iframe></div>
   )
 }
