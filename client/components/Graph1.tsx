a54ee7f0c69bd366844bb29d3031a54a830612f0
anthonyle910 <anthonyle910@yahoo.com>
2023-07-05 18:40:28 -0700

    added more graphs

--- client/components/Graph1.tsx
+++ client/components/Graph1.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Graph1() {
   return (
-    <div><iframe className='chart'src="http://localhost:3000/d/f849b578-124a-4e19-a781-12d838580e64/messagein?orgId=1&refresh=5s&viewPanel=1&kiosk"  frameBorder="0" ></iframe></div>
+    <div><iframe className='chart' src="http://localhost:3000/d/c2ef0e4a-da28-4797-bd86-5c1957aa9039/newmessagesin?orgId=1&refresh=5s&viewPanel=1&viewPanel=1&kiosk" frameBorder='0'></iframe></div>
   )
 }
+{/* <iframe className='chart'src="http://localhost:3000/d/f849b578-124a-4e19-a781-12d838580e64/messagein?orgId=1&refresh=5s&viewPanel=1&kiosk"  frameBorder="0" ></iframe> */}
--- client/components/Graph2.tsx
+++ client/components/Graph2.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Graph2() {
   return (
-    <div>Graph2</div>
+    <div><iframe className='chart' src="http://localhost:3000/d/b7cc76a7-51f0-4175-bbbc-dada7ec3fa2d/messagesout?orgId=1&refresh=5s&viewPanel=1&kiosk" frameBorder='0'></iframe></div>
   )
 }
