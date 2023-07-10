890f01fcfc3ff458c3a0bb22dcde903622cb6d05
cyduckk <cyrux@live.com>
2023-07-10 11:36:39 -0700

    fixing commit issues

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Body from './client/components/Body';
-import Navbar from './client/components/Navbar';
+import React, { useEffect } from 'react'
+import Body from './client/components/Body'
+import Navbar from './client/components/Navbar'
 // export default function App() {
 //   return (
 //     <div>App</div>
@@ -41,19 +41,30 @@ async function getTopics() {
 
 getTopics();
 
+
 const App = () => {
+  // const grafport: string | number = process.env.GRAFPORT ?? 2999;
+  // console.log(grafport);
+  // console.log('hello I am in app')
+  // (async function getGraf () {
+  //   console.log('in app.tsx')
+  //   const data = await fetch('/grafport');
+  //   const result = await data.text();
+  //   console.log(result)
+    
+    // .then(data => {
+    //   console.log(data)
+    // });
+    
+    // console.log(grafport.json())
+  // })();
+  // console.log('hello')
   return (
     <div className='AppDisplay'>
-      <div className='Navbar'>
-        {' '}
-        <Navbar />{' '}
-      </div>
-      <div className='Body'>
-        {' '}
-        <Body />{' '}
-      </div>
+       <div className='Navbar'> <Navbar /> </div>
+       <div className='Body'> <Body /> </div> 
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
