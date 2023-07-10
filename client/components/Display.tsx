6fed6f3284e8a8a707f052211dee454d8a65e765
cyduckk <cyrux@live.com>
2023-07-10 14:24:31 -0700

    is adding message rate graph

--- client/components/Display.tsx
+++ client/components/Display.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import Graph1 from './Graph1'
-import Graph2 from './Graph2'
-import Graph3 from './Graph3'
-import Graph4 from './Graph4'
+import React from 'react';
+import Graph1 from './Graph1';
+import Graph2 from './Graph2';
+import Graph3 from './Graph3';
+import Graph4 from './Graph4';
+import Graph5 from './Graph5';
 
 export default function Display() {
   return (
     <div className='DisplayContainer'>
-      <div id='Graph1' className='Graph'><Graph1/></div>
-      <div id='Graph2' className='Graph'><Graph2/></div>
-      <div id='Graph3' className='Graph'><Graph3/></div>
-      <div id='Graph4' className='Graph'><Graph4/></div>
+      <div id='Graph1' className='Graph'>
+        <Graph1 />
+      </div>
+      <div id='Graph2' className='Graph'>
+        <Graph2 />
+      </div>
+      <div id='Graph3' className='Graph'>
+        <Graph3 />
+      </div>
+      <div id='Graph5' className='Graph'>
+        <Graph5 />
+      </div>
+      <div id='Graph4' className='Graph'>
+        <Graph4 />
+      </div>
     </div>
-  )
+  );
 }
