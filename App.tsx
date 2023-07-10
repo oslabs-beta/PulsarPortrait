4cbf151a7e98d8d6b14269b7df0678a1c0e7d622
cyduckk <cyrux@live.com>
2023-07-10 11:15:34 -0700

    working on getting topics to show

--- App.tsx
+++ App.tsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import Body from './client/components/Body'
-import Navbar from './client/components/Navbar'
+import React from 'react';
+import Body from './client/components/Body';
+import Navbar from './client/components/Navbar';
 // export default function App() {
 //   return (
 //     <div>App</div>
 //   )
 // }
 
+// async function getTopics() {
+//   const result = await fetch(
+//     'http://localhost:8080/admin/v2/persistent/public/default',
+//     {
+//       mode: 'no-cors',
+//     }
+//   );
+//   console.log(result);
+// }
+// getTopics();
+async function getTopics() {
+  const response = await fetch(
+    'http://localhost:8080/admin/v2/persistent/public/default',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    console.log('Error retrieving topics:', response.status, errorMessage);
+    return;
+  }
+
+  const data = await response.json();
+  const topics = data.topics;
+
+  console.log('Topics:', topics);
+}
+
+getTopics();
 
 const App = () => {
   return (
     <div className='AppDisplay'>
-      
-       <div className='Navbar'> <Navbar /> </div>
-       <div className='Body'> <Body /> </div> 
+      <div className='Navbar'>
+        {' '}
+        <Navbar />{' '}
+      </div>
+      <div className='Body'>
+        {' '}
+        <Body />{' '}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -8,32 +8,44 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-// statically serve the build folder 
+// statically serve the build folder
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
 // app.use(express.static('client'));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:7080'); // Replace with your client origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Add other allowed methods if needed
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Add other allowed headers if needed
 
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 //route error handler
-app.use((req,res) => res.status(404).send('Error, not the page you are looking for'));
+app.use((req, res) =>
+  res.status(404).send('Error, not the page you are looking for')
+);
 
 // global error handler to handle erros within middleware
 app.use((err, req, res, next) => {
-    //Define a default error object
-    const defaultErr = {
-      log: 'Express error handler caught unknown middleware error',
-      status: 400,
-      message: { err: 'An error occured' },
-    };
-    // define an errorObj to combine new errors
-    const errObj = Object.assign(defaultErr, err);
-    console.log('Error: ', errObj.log);
-    // return to the client the status and error message
-    return res.status(errObj.status || 500).send(errObj.message);
-  });
+  //Define a default error object
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occured' },
+  };
+  // define an errorObj to combine new errors
+  const errObj = Object.assign(defaultErr, err);
+  console.log('Error: ', errObj.log);
+  // return to the client the status and error message
+  return res.status(errObj.status || 500).send(errObj.message);
+});
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
-
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
 
 module.exports = app;
