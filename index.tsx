77b7fa7afffd684f50475637872ddc25dc068138
Grant <grantct71@gmail.com>
2023-07-19 17:11:31 -0700

    cleans up compose file

--- index.tsx
+++ index.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-// import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
-// import App
-
-// in tsconfig, "allowSyntheticDefaultImports": true, was required to import styles like this
 import './client/styles/styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
